82ffe6082ba49ae62867cf99fc0df5949384f768
mably <f.mably@gmail.com>
2014-11-22 23:43:39 +0100

    SignBlock method added

--- ppc.go
+++ ppc.go
@@ -5,5 +5,65 @@
 package main
 
 import (
-
+	"fmt"
+	_ "github.com/conformal/btcec"
+	"github.com/mably/btcnet"
+	"github.com/mably/btcscript"
+	"github.com/mably/btcutil"
+	"github.com/mably/btcwire"
+	"github.com/mably/ppcwallet/keystore"
 )
+
+// SignBlock TODO(?)
+// ppc: sign block
+func SignBlock(
+	msgBlock *btcwire.MsgBlock, params *btcnet.Params, store *keystore.Store) error {
+
+	var txOut *btcwire.TxOut
+
+	if msgBlock.IsProofOfStake() {
+		txOut = msgBlock.Transactions[1].TxOut[1]
+	} else {
+		txOut = msgBlock.Transactions[0].TxOut[0]
+	}
+
+	scriptClass, addresses, _, err :=
+		btcscript.ExtractPkScriptAddrs(txOut.PkScript, params)
+	if err != nil {
+		return fmt.Errorf("cannot extract addresses: %v", err)
+	}
+
+	if scriptClass != btcscript.PubKeyTy {
+		return UnsupportedTransactionType
+	}
+
+	apk, ok := addresses[0].(*btcutil.AddressPubKey)
+	if !ok {
+		return UnsupportedTransactionType
+	}
+
+	ai, err := store.Address(apk)
+	if err != nil {
+		return fmt.Errorf("cannot get address info: %v", err)
+	}
+
+	pka := ai.(keystore.PubKeyAddress)
+	key, err := pka.PrivKey()
+	if err != nil {
+		return fmt.Errorf("cannot get private key: %v", err)
+	}
+
+	sha, err := msgBlock.BlockSha()
+	if err != nil {
+		return fmt.Errorf("cannot get block hash: %v", err)
+	}
+
+	signature, err := key.Sign(sha.Bytes())
+	if err != nil {
+		return fmt.Errorf("cannot sign block: %v", err)
+	}
+
+	msgBlock.Signature = signature.Serialize()
+
+	return nil
+}
